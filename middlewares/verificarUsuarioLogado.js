90f02aa8a4be2c3a011b8d54a3db1bc02c5087aa
Eduardo <galvaocruz16@gmail.com>
2021-07-28 14:53:28 -0300

    desafio concluído

--- middlewares/verificarUsuarioLogado.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(req, res, next) {
-  if (req.session.usuario) {
-    next();
-  } else {
-    res.redirect("/")
-  }
-}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,43 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const usuariosController = require('../controller/usuariosController');
+const verificarLogin = require('../middleware/verificarLogin')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  const { session } = req;
+  res.render('login')
 });
+router.get('/cadastro', (req,res,next)=>{
+  res.render('cadastro')
+});
+
+router.post('/cadastro', (req,res,next)=>{
+  const {nome,email,senha,confirmar} = req.body;
+  const usuario = usuariosController.criarUsuario(nome, email, senha,confirmar);
+  
+  req.session.usuario = usuario;
+  console.log(req.session.usuario);
+  res.render('produtos', { usuario: req.session.usuario});
+  
+})
+
+router.post("/login", function (req, res, next) {
+  const { email, senha} = req.body;
+  const usuario = usuariosController.validarUsuario(email,senha);
+  req.session.usuario=usuario;
+  res.render('produtos', { usuario: req.session.usuario});
+});
+router.get('/produtos', verificarLogin, function(req, res, next){
+ 
+
+  res.render('produtos', { usuario: req.session.usuario});
+});
+
+router.get('/logout', function(req, res, next){
+  req.session.destroy;
+  res.redirect('/')
+});
+
 
 module.exports = router;
